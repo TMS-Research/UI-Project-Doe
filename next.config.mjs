16dca3e2df18043a16ca6b7afccb6d0bc0e34ace
danynurf <bydany.nf@gmail.com>
2025-04-13 11:50:06 +0700

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compress: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
