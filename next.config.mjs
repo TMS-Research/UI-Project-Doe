b9135e9376f3113d28f1b79b4e25038a7c062c6c
danynurf <bydany.nf@gmail.com>
2025-04-29 23:11:18 +0700

    update cd & next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true,
+  generateBuildId: () => {
+    return Date.now().toString();
+  },
   images: {
     remotePatterns: [
       {
